fe07d545a2dfe45dc63a07ed602a05cf03689bf2
danwill <daniel.willett2@gmail.com>
2023-03-10 13:38:58 -0800

    fixed route to remove a reaction by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -122,7 +122,7 @@ module.exports = {
   removeReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
-      { $pull: { reactions: reactionId } },
+      { $pull: { reactions: {_id: req.params.reactionId } }},
       { new: true }
     )
       .then((thought) => {
